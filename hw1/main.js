f1e94341569d1741f686368ae07794238578d824
ArtPro1711 <lukaichuk03@gmail.com>
2023-05-01 22:24:26 +0300

    fst

---
+++ hw1/main.js
@@ -0,0 +1,49 @@
+let a = 'hello'
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let e = 1
+let f = 10
+let g = -999
+let h = 123
+let i = 3.14
+let j = 2.7
+let k = 16
+let l = true
+let m = false
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+
+let firstName = 'Lukaichuk'
+let middleName = 'Artur'
+let lastName = 'Anatoliyovich'
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+
+
+let n = 100
+let o = '100'
+let p = true
+console.log(typeof n);
+console.log(typeof o);
+console.log(typeof p);
+
+
+let name = prompt("Ваше ім'я")
+let patronymic = prompt("По-Батькові")
+let age = prompt("Скільки років")
+console.log(name);
+console.log(patronymic);
+console.log(age);
